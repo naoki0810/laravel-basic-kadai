8253302bb9ae54739e2689745ff10f45f555fb2a
naoki0810 <naoki.komiya.98ss@gamil.com>
2023-12-17 23:01:28 +0900

    課題提出

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,9 @@ use App\Http\Controllers\PostController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function () {
+  return view('welcome');
+});
 
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);  
